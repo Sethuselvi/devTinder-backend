c8fb2792b2e18ca761dba1cc44c265f0ef04c743
Sethuselvi <sethuselvi-98@gmail.com>
2026-01-27 17:02:46 +0530

    Handling different incoming requests with server

--- src/app.js
+++ src/app.js
@@ -3,6 +3,14 @@ const app = express();
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
-app.use((req,res)=>{
+app.use("/test",(req,res)=>{
     res.send("Hello from server!!")
+})
+
+app.use("/hello",(req,res)=>{
+    res.send("Helo hello  hello!!")
+})
+
+app.use("/",(req,res)=>{
+    res.send("Hello from dashboard!!")
 })
