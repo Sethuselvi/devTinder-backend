f1d5680fcf5db72927626d2c459e49c5a667dd37
Sethuselvi <sethuselvi-98@gmail.com>
2026-02-02 20:05:52 +0530

    Tested different route regex patterns in express 5

--- src/app.js
+++ src/app.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const app = express();
-//Overrides http methods
-// app.use("/user",(req,res)=>{
-//     res.send("HAHAHA")
+
+// //Testing regex pattern routes
+//Route pattern uses express 5 syntax
+// Works with /ac,/abc
+//Express 4(older version pattern) "ab?c"
+app.get("/a{b}c",(req,res)=>{
+    res.send("Testing optional pattern")
+})
+
+// // Works with /abc ,/abbbc
+//Express 4(older version pattern) "ab+c"
+// app.get(/^\/ab+c$/,(req,res)=>{
+//     res.send("Testing + pattern")
 // })
+
+// Works with /abc ,/absethuselvic
+//Express 4(older version pattern) "ab*c"
+app.get(/^\/ab.*c$/,(req,res)=>{
+    res.send("Testing * pattern")
+})
+
+// // Works with /ad ,/abcd,/abcbcbcd
+//Express 4(older version pattern) "a(bc)?d"
+// app.get(/^\/a(bc).*d$/,(req,res)=>{
+//     res.send("Testing option group pattern")
+// })
+
+
 
 app.get("/user",(req,res)=>{
     res.send({firstName:"sethu",lastName:"selvi"})
