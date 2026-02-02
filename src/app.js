45a8cc36883a6c0cc72698ed83504d7588ddfcaf
Sethuselvi <sethuselvi-98@gmail.com>
2026-02-02 13:34:31 +0530

    Using express http methods on same url

--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const app = express();
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+//Overrides http methods
+// app.use("/user",(req,res)=>{
+//     res.send("HAHAHA")
+// })
+
+app.get("/user",(req,res)=>{
+    res.send({firstName:"sethu",lastName:"selvi"})
+})
+app.post("/user",(req,res)=>{
+    res.send("Data successfully saved to database")
+})
+app.delete("/user",(req,res)=>{
+    res.send("Deleted successfully")
 })
 app.use("/test",(req,res)=>{
     res.send("Hello from server!!")
 })
 
-app.use("/hello",(req,res)=>{
-    res.send("Helo hello  hello hello !!")
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
 })
 
-app.use("/",(req,res)=>{
-    res.send("Hello from dashboard!!")
-})
+
+
+// app.use("/",(req,res)=>{
+//     res.send("Hello from dashboard!!")
+// })
