1b48794966a6a1e081a206ac5b7c3832533b7609
Sethuselvi <sethuselvi-98@gmail.com>
2026-02-03 19:08:19 +0530

    Tested responses with multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,53 +1,42 @@
 const express = require('express');
 const app = express();
+// app.use("/user",(req,res,next) => {
+//     console.log("Handling route User 1 !");
+//     next();
+//      res.send("1st Response")
+// },
+// (req,res) => {
+//     console.log("Handling route User 2 !");
+//     res.send("2nd Response")
+// }
+// )
 
-// //Testing regex pattern routes
-//Route pattern uses express 5 syntax
-// Works with /ac,/abc
-//Express 4(older version pattern) "ab?c"
-app.get("/a{b}c",(req,res)=>{
-    res.send("Testing optional pattern")
-})
+app.use("/user",(req,res,next) => {
+    console.log("Handling route User 1 !");
+    next();
+    //  res.send("1st Response")
+},
+(req,res,next) => {
+    console.log("Handling route User 2 !");
+    next();
+    // res.send("2nd Response")
+},
+[(req,res,next) => {
+    console.log("Handling route User 3 !");
+    next();
+    // res.send("3rd Response")
+},
+(req,res,next) => {
+    console.log("Handling route User 4 !");
+    next();
+    // res.send("4th Response")
+}],
+(req,res,next) => {
+    console.log("Handling route User 5 !");
+    res.send("5th Response")
+}
+)
 
-// // Works with /abc ,/abbbc
-//Express 4(older version pattern) "ab+c"
-// app.get(/^\/ab+c$/,(req,res)=>{
-//     res.send("Testing + pattern")
-// })
-
-// Works with /abc ,/absethuselvic
-//Express 4(older version pattern) "ab*c"
-app.get(/^\/ab.*c$/,(req,res)=>{
-    res.send("Testing * pattern")
-})
-
-// // Works with /ad ,/abcd,/abcbcbcd
-//Express 4(older version pattern) "a(bc)?d"
-// app.get(/^\/a(bc).*d$/,(req,res)=>{
-//     res.send("Testing option group pattern")
-// })
-
-
-
-app.get("/user",(req,res)=>{
-    res.send({firstName:"sethu",lastName:"selvi"})
-})
-app.post("/user",(req,res)=>{
-    res.send("Data successfully saved to database")
-})
-app.delete("/user",(req,res)=>{
-    res.send("Deleted successfully")
-})
-app.use("/test",(req,res)=>{
-    res.send("Hello from server!!")
-})
-
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("Server is running on port 3000");
 })
-
-
-
-// app.use("/",(req,res)=>{
-//     res.send("Hello from dashboard!!")
-// })
