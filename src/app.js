b8109936a50513c3022340356c1d5d4e534f7aa1
Sethuselvi <sethuselvi-98@gmail.com>
2026-02-06 16:16:27 +0530

    Made database connection first and then server listening to requests

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/database');
 
-require('./config/database');
-app.listen(3000, () => {
+
+connectDB().then(()=>{
+    console.log("Dtabase Connection established...")
+    app.listen(3000, () => {
     console.log("Server is running on port 3000");
 })
+}).catch((err)=>{
+    console.error("Database connection cannot be established")
+})
+
+
+
