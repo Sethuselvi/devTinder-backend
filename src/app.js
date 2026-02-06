c752bc055e40f1bf04e5049181c4708009816527
Sethuselvi <sethuselvi-98@gmail.com>
2026-02-06 19:16:24 +0530

    Created a post api to add and save hardcoded  user  details in mongodb database

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/database');
+const User= require('./models/user');
 
-
+app.post('/signup',async (req,res) =>{
+    //Creating new instance of user model
+    const user = new User({
+        firstName:"Sachin",
+        lastName:"Tendular",
+        emailId:"sachintendulkar@gmail.com",
+        password:"sethu@1230"
+    })
+    try{
+        await user.save()
+        res.send('User Added successfully')
+    }
+    catch(err){
+        res.send(400).send("Error saving the user:" + err.message)
+    }
+    
+})
 connectDB().then(()=>{
     console.log("Database Connection established...")
     app.listen(3000, () => {
